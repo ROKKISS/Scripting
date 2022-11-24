d385540dc09b4798cebe2354b66fb3fd816d7893
ROKKISS <118459018+ROKKISS@users.noreply.github.com>
2022-11-24 20:00:00 +0500

    123
    123

---
+++ NPC.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int health = 10;
+        int level = 1;
+            int newSpeed = 10;
+            health = health + newSpeed;
+        print("health = health + newSpeed");
+        print("Message");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {float speed =1.2f
+        
+    }
+}
